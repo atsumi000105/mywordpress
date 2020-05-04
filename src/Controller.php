elementor/wp2static

commit 09847fdc92faa82ab4ddd9375b17aaebb1aec580

add queueing actions only when required

src/Controller.php
@@ -8,7 +8,7 @@ use WP_CLI;
 use WP_Post;
 
 class Controller {
-    const WP2STATIC_VERSION = '7.0-alpha-006';
+    const WP2STATIC_VERSION = '7.0-alpha-007';
 
     /**
      * @var string

src/WordPressAdmin.php
@@ -202,22 +202,6 @@ class WordPressAdmin {
                 2
             );
         }
-
-        if ( CoreOptions::getValue( 'queueJobOnPostSave' ) ) {
-            add_action(
-                'save_post',
-                [ 'WP2Static\Controller', 'wp2static_save_post_handler' ],
-                0
-            );
-        }
-
-        if ( CoreOptions::getValue( 'queueJobOnPostDelete' ) ) {
-            add_action(
-                'trashed_post',
-                [ 'WP2Static\Controller', 'wp2static_trashed_post_handler' ],
-                0
-            );
-        }
     }
 
     /**

src/CoreOptions.php
@@ -406,17 +406,48 @@ class CoreOptions {
 
                 break;
             case 'jobs':
+                $queue_on_post_save = isset( $_POST['queueJobOnPostSave'] ) ? 1 : 0;
+                $queue_on_post_delete = isset( $_POST['queueJobOnPostDelete'] ) ? 1 : 0;
+
                 $wpdb->update(
                     $table_name,
-                    [ 'value' => isset( $_POST['queueJobOnPostSave'] ) ? 1 : 0 ],
+                    [ 'value' => $queue_on_post_save ],
                     [ 'name' => 'queueJobOnPostSave' ]
                 );
 
                 $wpdb->update(
                     $table_name,
-                    [ 'value' => isset( $_POST['queueJobOnPostDelete'] ) ? 1 : 0 ],
+                    [ 'value' => $queue_on_post_delete ],
                     [ 'name' => 'queueJobOnPostDelete' ]
                 );
+
+                if ( $queue_on_post_save ) {
+                    add_action(
+                        'save_post',
+                        [ 'WP2Static\Controller', 'wp2static_save_post_handler' ],
+                        0
+                    );
+                } else {
+                    remove_action(
+                        'save_post',
+                        [ 'WP2Static\Controller', 'wp2static_save_post_handler' ],
+                        0
+                    );
+                }
+
+                if ( $queue_on_post_delete ) {
+                    add_action(
+                        'trashed_post',
+                        [ 'WP2Static\Controller', 'wp2static_trashed_post_handler' ],
+                        0
+                    );
+                } else {
+                    remove_action(
+                        'trashed_post',
+                        [ 'WP2Static\Controller', 'wp2static_trashed_post_handler' ],
+                        0
+                    );
+                }
 
                 $process_queue_interval =
                     isset( $_POST['processQueueInterval'] ) ?
@@ -428,8 +459,6 @@ class CoreOptions {
                     [ 'name' => 'processQueueInterval' ]
                 );
 
-                // TODO: this looks like odd value passed in
-                // TODO: this WPCron method prints output in error_log
                 WPCron::setRecurringEvent( $process_queue_interval );
 
                 $wpdb->update(

wp2static.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP2Static
  * Plugin URI:  https://wp2static.com
  * Description: Static site generator functionality for WordPress.
- * Version:     7.0-alpha-006
+ * Version:     7.0-alpha-007
  * Author:      WP2Static
  * Author URI:  https://wp2static.com
  * Text Domain: static-html-output-plugin

atsumi000105/mywordpress

commit 1708614c61ec03d16302f5408fb8cba511847056

Fix job queueing when a post is saved or trashed.
This fixes a regression introduced in 09847fdc92faa82ab4ddd9375b17aaebb1aec580,
but rather than simply revert the relevant changes made there, I opted to
move the calls to CoreOptions::getValue into the handlers, so they will
only hit the DB when the hooks fire, rather than potentially every page load.

src/Controller.php
@@ -395,15 +395,16 @@ class Controller {
     }
 
     public static function wp2static_save_post_handler( int $post_id ) : void {
-        if ( get_post_status( $post_id ) !== 'publish' ) {
-            return;
+        if ( CoreOptions::getValue( 'queueJobOnPostSave' ) &&
+             get_post_status( $post_id ) === 'publish' ) {
+            self::wp2static_enqueue_jobs();
         }
-
-        self::wp2static_enqueue_jobs();
     }
 
     public static function wp2static_trashed_post_handler() : void {
-        self::wp2static_enqueue_jobs();
+        if ( CoreOptions::getValue( 'queueJobOnPostDelete' ) ) {
+            self::wp2static_enqueue_jobs();
+        }
     }
 
     public static function wp2static_enqueue_jobs() : void {

src/WordPressAdmin.php
@@ -185,6 +185,18 @@ class WordPressAdmin {
             1
         );
 
+        add_action(
+            'save_post',
+            [ 'WP2Static\Controller', 'wp2static_save_post_handler' ],
+            0
+        );
+
+        add_action(
+            'trashed_post',
+            [ 'WP2Static\Controller', 'wp2static_trashed_post_handler' ],
+            0
+        );
+
         /*
          * Register actions for when we should invalidate cache for
          * a URL(s) or whole site